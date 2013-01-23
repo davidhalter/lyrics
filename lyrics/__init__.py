7a18e0a22b209df213279385be6d3f0e5dea191f
David Halter <davidhalter88@gmail.com>
2013-01-23 15:36:34 +0100

    package init

---
+++ lyrics/__init__.py
@@ -0,0 +1,8 @@
+" a -- Console radio player. "
+
+version_info = (0, 0, 1)
+
+__version__ = version = '.'.join(map(str, version_info))
+__project__ = __name__
+__author__ = "David Halter"
+__license__ = "GPL 3"
