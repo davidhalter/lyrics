5960357f2c9118d4f89cefc32a6236e94cc2dc51
David Halter <davidhalter88@gmail.com>
2013-01-23 15:32:30 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+from lyrics import version, __project__, __license__
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+
+install_requires = ['docopt']
+
+
+meta = dict(
+    name=__project__,
+    version=version,
+    license=__license__,
+    description='Play music and view the lyrics.',
+    long_description=readme,
+    platforms=('Any'),
+
+    author='David Halter',
+    url=' http://github.com/davidhalter/lyrics',
+
+    packages=find_packages(),
+    include_package_data=True,
+
+    entry_points={
+        'console_scripts': [
+            'lyrics = lyrics.main',
+        ]
+    },
+
+    install_requires=install_requires,
+    test_suite = 'test',
+)
+
+
+if __name__ == "__main__":
+    setup(**meta)
