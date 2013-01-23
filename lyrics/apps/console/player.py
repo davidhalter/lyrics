8b243821f86e8c8dced74eb46dd71266c59f220a
David Halter <davidhalter88@gmail.com>
2013-01-23 22:21:57 +0100

    modified player -> removed class

--- lyrics/apps/console/player.py
+++ lyrics/apps/console/player.py
@@ -1,77 +1,76 @@
-""" taken from pyradio """
+"""
+taken from pyradio and modified
+Playing is handled by mplayer
+"""
 
 import subprocess
 import thread
 import os
 
 
-class Player(object):
-    """ Media player class. Playing is handled by mplayer """
-    process = None
+process = None
+last_status = ''
 
-    def __init__(self, outputStream):
-        self.outputStream = outputStream
 
-    def __del__(self):
-        self.close()
+def status():
+    try:
+        user_input = process.stdout.readline()
+        while(user_input != ''):
+            outputStream.write(user_input)
+            user_input = process.stdout.readline()
+    except:
+        return None
 
-    def updateStatus(self):
+def is_playing():
+    return bool(process)
+
+def play(uri):
+    """ use mplayer to play a stream """
+    global process
+    close()
+    file_name = uri.split("?")[0] if uri.starts_with('http://') else uri
+
+    is_play_list = file_name[-4:] in ('.m3u', '.pls')
+
+    opts = ["mplayer", "-quiet", uri]
+    if is_play_list:
+        opts.insert(2, '-playlist')
+
+    process = subprocess.Popen(opts, shell=False,
+                               stdout=subprocess.PIPE,
+                               stdin=subprocess.PIPE,
+                               stderr=subprocess.STDOUT)
+    thread.start_new_thread(updateStatus, ())
+
+def sendCommand(command):
+    """ send keystroke command to mplayer """
+    if(process is not None):
         try:
-            user_input = self.process.stdout.readline()
-            while(user_input != ''):
-                self.outputStream.write(user_input)
-                user_input = self.process.stdout.readline()
+            process.stdin.write(command)
         except:
             pass
 
-    def is_playing(self):
-        return bool(self.process)
+def mute():
+    """ mute mplayer """
+    sendCommand("m")
 
-    def play(self, uri):
-        """ use mplayer to play a stream """
-        self.close()
-        file_name = uri.split("?")[0] if uri.starts_with('http://') else uri
+def pause():
+    """ pause streaming (if possible) """
+    sendCommand("p")
 
-        is_play_list = file_name[-4:] in ('.m3u', '.pls')
+def close():
+    """ exit pyradio (and kill mplayer instance) """
+    global process
+    sendCommand("q")
+    if process is not None:
+        os.kill(process.pid, 15)
+        process.wait()
+    process = None
 
-        opts = ["mplayer", "-quiet", uri]
-        if is_play_list:
-            opts.insert(2, '-playlist')
+def volumeUp():
+    """ increase mplayer's volume """
+    sendCommand("*")
 
-        self.process = subprocess.Popen(opts, shell=False,
-                                        stdout=subprocess.PIPE,
-                                        stdin=subprocess.PIPE,
-                                        stderr=subprocess.STDOUT)
-        thread.start_new_thread(self.updateStatus, ())
-
-    def sendCommand(self, command):
-        """ send keystroke command to mplayer """
-        if(self.process is not None):
-            try:
-                self.process.stdin.write(command)
-            except:
-                pass
-
-    def mute(self):
-        """ mute mplayer """
-        self.sendCommand("m")
-
-    def pause(self):
-        """ pause streaming (if possible) """
-        self.sendCommand("p")
-
-    def close(self):
-        """ exit pyradio (and kill mplayer instance) """
-        self.sendCommand("q")
-        if self.process is not None:
-            os.kill(self.process.pid, 15)
-            self.process.wait()
-        self.process = None
-
-    def volumeUp(self):
-        """ increase mplayer's volume """
-        self.sendCommand("*")
-
-    def volumeDown(self):
-        """ decrease mplayer's volume """
-        self.sendCommand("/")
+def volumeDown():
+    """ decrease mplayer's volume """
+    sendCommand("/")
