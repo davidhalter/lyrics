173b94b3686922658fa9e37b1637e7a2a84b28f1
David Halter <davidhalter88@gmail.com>
2013-01-24 16:40:01 +0100

    big refactoring, to make the app readable

--- lyrics/apps/console/app.py
+++ lyrics/apps/console/app.py
@@ -10,10 +10,13 @@ import debug
 
 class States(object):
     """State machine of the app"""
-    def __init__(self):
+    def __init__(self, playlist):
+        self.playlist = playlist
+
         self.split_screen = True
         self._show_help = True
         self.playing = None
+        self.current_window = None
 
     @property
     def show_help(self):
@@ -26,17 +29,55 @@ class States(object):
         self._show_help = value
 
 
-class App(object):
+class Window(object):
+    def __init__(self, states, x, y, width, height):
+        debug.debug('new_win', self.__class__.__name__, x, y, width, height)
+
+        self.states = states
+        self.win_curses = curses.newwin(height, width, y, x)
+        self.height, self.width = self.win_curses.getmaxyx()
+        self.init()
+        self.draw()
+
+    def init(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def clean_position(self, x, y, float_floor=False):
+        width, height = self.width, self.height
+        if isinstance(x, float):
+            x = int(width * x)
+            if float_floor:
+                x += 1
+        if isinstance(y, float):
+            y = int(height * y)
+            if float_floor:
+                y += 1
+
+        if x is None:
+            x = width
+        if y is None:
+            y = height
+
+        if x < 0:
+            x = width + x
+        if y < 0:
+            y = height + y
+        return x, y
+
+
+class App(Window):
     def __init__(self, path):
-        self.playlist = playlist.Playlist.from_path(path)
-        self.states = States()
+        p = playlist.Playlist.from_path(path)
+        self.states = States(p)
 
     def start(self):
-        curses.wrapper(self.setup)  # must be called at the end (loop)
+        curses.wrapper(self.setup)  # the infinite loop
 
     def setup(self, stdscr):
         self.stdscr = stdscr
-
         try:
             curses.curs_set(0)
         except:
@@ -53,137 +94,115 @@ class App(object):
         curses.init_pair(9, curses.COLOR_BLACK, curses.COLOR_GREEN)
 
         self.stdscr.nodelay(0)
-        self.draw_screen()
+        self.draw()
 
         self.run()
 
     def run(self):
         while True:
             try:
-                c = self.window_list.getkey()
+                c = self.states.current_window.win_curses.getkey()
                 if c == 'KEY_RESIZE':
-                    self.draw_screen()
+                    self.draw()
                     continue
                 keys.execute_event(self, c)
             except KeyboardInterrupt:
                 break
 
-    def draw_screen(self):
-        self.max_y, self.max_x = self.stdscr.getmaxyx()
+    def create_window(self, cls, x, y , width, height):
+        x, y = self.clean_position(x, y, float_floor=True)
+        width, height = self.clean_position(width, height)
+        return cls(self.states, x, y, width, height)
 
-        self.window_head = self._new_win(0, 0, None, 1)
+    def move_cursor(self, y, x=0):
+        return self.states.current_window.move_cursor(y, x)
+
+    def draw(self):
+        self.height, self.width = self.stdscr.getmaxyx()
+
+        self.head = self.create_window(Head, 0, 0, None, 1)
         if self.states.split_screen:
             golden = 0.382
-            self.window_list = self._new_win(0, 1, golden, -2)
-            self.window_lyrics = self._new_win(golden, 1, 1 - golden, -2)
+            self.song_list = self.create_window(SongList, 0, 1, golden, -2)
+            self.lyrics = self.create_window(Lyrics, golden, 1, 1 - golden, -2)
         else:
-            self.window_list = self._new_win(0, 1, None, -2)
-        self.window_footer = self._new_win(0, -1, None, 1)
-
-        self.init_head()
-        self.init_list()
-        if self.states.split_screen:
-            self.init_lyrics()
-        self.init_footer()
-
-        #self.stdscr.timeout(100)
-        self.window_list.keypad(1)
-
-        #self.stdscr.noutrefresh()
+            self.song_list = self.create_window(SongList, 0, 1, None, -2)
+        self.footer = self.create_window(Footer, 0, -1, None, 1)
+        self.states.current_window = self.song_list
 
         curses.doupdate()
 
-    def clean_position(self, x, y, type='main', float_floor=False):
-        if type == 'main':
-            max_x, max_y = self.max_x, self.max_y
-        elif type == 'list':
-            max_x, max_y = self.max_list_x, self.max_list_y
 
-        if isinstance(x, float):
-            x = int(max_x * x)
-            if float_floor:
-                x += 1
-        if isinstance(y, float):
-            y = int(max_y * y)
-            if float_floor:
-                y += 1
-
-        if x is None:
-            x = max_x
-        if y is None:
-            y = max_y
-
-        if x < 0:
-            x = max_x + x
-        if y < 0:
-            y = max_y + y
-        return x, y
-
-    def _new_win(self, x, y, width=None, height=None):
-        x, y = self.clean_position(x, y, float_floor=True)
-        width, height = self.clean_position(width, height)
-
-        debug.debug('new_win', x, y, width, height)
-        return curses.newwin(height, width, y, x)
-
-    def init_head(self):
+class Head(Window):
+    def init(self):
         info = " lyrics "
-        self.window_head.addstr(0, 0, info, curses.color_pair(4))
+        self.win_curses.addstr(0, 0, info, curses.color_pair(4))
         right_str = "test"
         x, y = self.clean_position(- len(right_str) - 1, 0)
         debug.debug('test', x, y, self.clean_position(None, None))
-        self.window_head.addstr(y, x, right_str, curses.color_pair(2))
-        self.window_head.bkgd(' ', curses.color_pair(7))
-        self.window_head.noutrefresh()
+        self.win_curses.addstr(y, x, right_str, curses.color_pair(2))
+        self.win_curses.bkgd(' ', curses.color_pair(7))
+        self.win_curses.noutrefresh()
 
-    def init_lyrics(self):
-        self.max_lyrics_y, self.max_lyrics_x = self.window_lyrics.getmaxyx()
-        self.window_lyrics.noutrefresh()
-        self.draw_lyrics()
+
+class Footer(Window):
+    def init(self):
+        self.win_curses.bkgd(' ', curses.color_pair(7))
+        self.win_curses.noutrefresh()
+
+
+class StatusLine(Window):
+    def init(self):
+        self.win_curses.bkgd(' ', curses.color_pair(7))
+        self.win_curses.noutrefresh()
+
+
+class Lyrics(Window):
+    def init(self):
+        self.win_curses.noutrefresh()
 
     def draw_lyrics(self):
-        self.window_lyrics.erase()
-        self.window_lyrics.box()
+        self.win_curses.erase()
+        self.win_curses.box()
 
-        self.window_lyrics.move(1, 1)
+        self.win_curses.move(1, 1)
         length, max_display = self.clean_position(-2, -2, 'list')
 
-    def init_list(self):
-        self.max_list_y, self.max_list_x = self.window_list.getmaxyx()
-        self.window_list.noutrefresh()
-        self.draw_song_list()
+    def _show_lyrics(self):
+        print(lyrics.get('Mumford & Sons', 'Sigh no more'))
 
-    def draw_song_list(self):
-        self.window_list.erase()
-        self.window_list.box()
 
-        self.window_list.move(1, 1)
+class SongList(Window):
+    def list(self):
+        self.max_list_y, self.max_list_x = self.win_curses.getmaxyx()
+        self.win_curses.noutrefresh()
+        self.win_curses.keypad(1)
+
+    def draw(self):
+        self.win_curses.erase()
+        self.win_curses.box()
+
+        self.win_curses.move(1, 1)
         length, max_display = self.clean_position(-2, -2, 'list')
-        for i, song in enumerate(self.playlist.visible_in_window(max_display)):
-            if song == self.states.playing and song == self.playlist.selected:
+        playlist = self.states.playlist
+        for i, song in enumerate(playlist.visible_in_window(max_display)):
+            if song == self.states.playing and song == playlist.selected:
                 col = curses.color_pair(9)
-            elif song == self.playlist.selected:
+            elif song == playlist.selected:
                 col = curses.color_pair(6)
             elif song == self.states.playing:
                 col = curses.color_pair(4)
             else:
                 col = curses.color_pair(5)
 
-            if song == self.states.playing or song == self.playlist.selected:
-                self.window_list.hline(i + 1, 1, ' ', length, col)
+            if song == self.states.playing or song == playlist.selected:
+                self.win_curses.hline(i + 1, 1, ' ', length, col)
 
-            self.window_list.addstr(i + 1, 1, song.format(length), col)
+            self.win_curses.addstr(i + 1, 1, song.format(length), col)
 
-        self.window_list.refresh()
+        self.win_curses.refresh()
 
-    def move_cursor(self, count, horizontal_count=0):
+    def move_cursor(self, y, x=0):
         # later we could also check for other lists here.
-        self.playlist.move_selected(count)
-        self.draw_song_list()
-
-    def init_footer(self):
-        self.window_footer.bkgd(' ', curses.color_pair(7))
-        self.window_footer.noutrefresh()
-
-    def _show_lyrics(self):
-        print(lyrics.get('Mumford & Sons', 'Sigh no more'))
+        self.states.playlist.move_selected(y)
+        self.draw()
